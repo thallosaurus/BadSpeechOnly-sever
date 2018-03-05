a88b2ec106db700a3d0df32cfaa5e14423b80cdf
Jarvis <Jarvis>
2018-03-05 14:52:43 +0100

    Fixed Data Distribution, added dist folder, added a small demo app, added features to testing tool (index.html) and other changes I can't recall now

--- app.js
+++ app.js
@@ -22,51 +22,73 @@ class _BSOPlayer {
 class _BSOServer {
 	constructor(port) {
 		this.port = port;
-		console.log("Constructed Server");
+		this.running = false;
 	}
 	
 	start() {
 		this._BSOInit();
 		http.listen(this.port)
-		console.log();
-		console.log("Server is running at localhost:"+this.port);
 	}
 	
 	_BSOInit() {
 		this.io = require("socket.io")(http);
-		var slots = new Array();
-		
+		console.log("Server is running at localhost:"+this.port);
+		this.running = true;
+
+		//var slots = new Array();
+		var slots = {};
+		var clients = new Array();
+
 		app.get("/", function (req, res) {
 			res.sendFile(__dirname+"\\index.html");
+		});
+
+		app.get("/dist/:files", function(req, res) {
+			res.sendFile(__dirname+"\\dist\\"+req.params.files);
 		});
 		
 		//So, we connected to our server
 		this.io.on("connection", function(socket) {
+			var id = socket.conn.id;
+			
 			//Then, after we requested the identity, we can set up the answer event:
 			console.log("Client connected, id: " + socket.conn.id);
 			
 			socket.on("player_create", function(data) {
 				var p = new _BSOPlayer(data);
-				var id = socket.conn.id;
 				if (slots[id] == null) {
 					slots[id] = p;
+					clients.push(id);
+					socket.broadcast.emit("broadcast", {clients, slots});
+					socket.emit("broadcast", {clients, slots});
 					console.log("created new player");
 				}
-				console.log(slots);
 			});
 			
 			socket.on("update", function (updateData) {
-				var id = socket.conn.id;
 				if (!(slots[id] == null)) {
 					slots[socket.conn.id].coordinates = updateData;
-					//.p.coordinates = updateData;
-					console.log(slots);
+
+					//broadcast to all clients except sender...
+					socket.broadcast.emit("broadcast", {clients, slots});
+					//...and to the sender.
+					socket.emit("broadcast", {clients, slots});
 				}
 			});
-				
+
+			//Manual Update
+			socket.on("pullSlots", function() {
+				if (!(slots[id] == null)) {
+					console.log("Sent player slots to " + id);
+					socket.emit("pushSlots", {clients, slots});
+				}
+			});
+
 			socket.on("disconnect", function() {
-				delete slots[socket.conn.id];
-				console.log(socket.conn.id + " disconnected");
+				delete slots[id];
+				delete clients[clients.indexOf(id)];
+				socket.broadcast.emit("broadcast", {clients, slots});
+				console.log(id + " disconnected");
 			});
 		});
 	}
